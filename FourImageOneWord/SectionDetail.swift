51dea2b9901926aca933e520a515602dc667a733
yasemin salan <yaseminsalann@gmail.com>
2021-07-30 15:42:35 +0300

    Amarena Software şirketindeki çalışmam . Şeyma Nur Aksoy

--- FourImageOneWord/SectionDetail.swift
+++ FourImageOneWord/SectionDetail.swift
@@ -31,7 +31,7 @@ struct SectionDetail: View {
     var body: some View {
         
         VStack(spacing:5){
-            
+            //kdjfdskdj
             ForEach(sectionListModel.sectionRowModel){ task in
                 ZStack{
                     HStack{
@@ -246,11 +246,7 @@ struct SectionDetail: View {
     
 }
 
-func convertBase64StringToImage (imageBase64String:String) -> UIImage {
-    let imageData = Data.init(base64Encoded: imageBase64String, options: .init(rawValue: 0))
-    let image = UIImage(data: imageData!)
-    return image!
-}
+
 
 struct SectionDetail_Previews: PreviewProvider {
     var model : ModelJson
